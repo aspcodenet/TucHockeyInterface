7184400af2013213d3e5714f833983059cddf4d3
aspcodenet <stefan.holmberg@systementor.se>
2023-03-08 11:16:39 +0100

    a

--- TucHockeyInterface/App.cs
+++ TucHockeyInterface/App.cs
@@ -18,6 +18,31 @@ public class App
 {
     public void Run()
     {
-        
+        IHockeyPlayerRepository repository;
+        while (true)
+        {
+            Console.WriteLine("1. Add player");
+            Console.WriteLine("2. List players");
+            Console.WriteLine("3. Avsluta");
+            var sel = Console.ReadLine();
+            if (sel == "3") break;
+            if (sel == "2")
+            {
+                foreach (var p in repository.ListAllPlayers())
+                {
+                    Console.WriteLine($" {p.Name} {p.Age}");
+                }
+            }
+
+            if (sel == "1")
+            {
+                Player player = new Player();
+                Console.WriteLine("Namn:")
+                player.Name = Console.ReadLine();
+                Console.WriteLine("Age:")
+                player.Age = Convert.ToInt32(Console.ReadLine());
+                repository.AddPlayer(player);
+            }
+        }
     }
 }
