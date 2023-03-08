d7b517b460ab553106dbfe2478327a4f009566e0
aspcodenet <stefan.holmberg@systementor.se>
2023-03-08 11:09:05 +0100

    Add project files.

---
+++ TucHockeyInterface/App.cs
@@ -0,0 +1,23 @@
+﻿
+public class Player
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Jersey { get; set; }
+}
+
+public interface IHockeyPlayerRepository
+{
+    void AddPlayer(Player player);
+    void UpdatePlayer(Player player);
+    List<Player> ListAllPlayers();
+}
+
+public class App
+{
+    public void Run()
+    {
+        
+    }
+}
